f98630e70d5b3245ca15221a6bb50d2339ce867f
Kavika <kbpalletenne@gmail.com>
2021-10-17 20:32:01 +1100

    code comments

--- src/user.rs
+++ src/user.rs
@@ -3,6 +3,26 @@ use crate::models::User;
 use sqlx::*;
 use std::env;
 
+
+
+//////////////////
+// CRUD Functions
+/////////////////
+
+// Create
+pub async fn create_user_by_id(id: i64, synergetic_user_id: i32, mgs_email: &str, mgs_password: &str) -> Result<()> {
+    let pool = establish_database_connection().await?;
+
+    sqlx::query!(
+        "INSERT INTO users (id, synergetic_user_id, mgs_email, mgs_password)
+         VALUES ($1, $2, $3, $4)",
+        id, synergetic_user_id, mgs_email, mgs_password
+    ).execute(&pool).await?;
+
+    Ok(())
+}
+
+// Read
 pub async fn get_user_by_id(id: i64) -> Result<User> {
     let pool = establish_database_connection().await?;
 
@@ -19,30 +39,7 @@ pub async fn get_user_by_id(id: i64) -> Result<User> {
     Ok(user)
 }
 
-pub async fn get_user_synergetic_id_by_id(id: i64) -> Result<i32> {
-    let pool = establish_database_connection().await?;
-
-    let record = sqlx::query!(
-        r#"SELECT synergetic_user_id FROM users WHERE id = $1"#,
-            id
-        )
-        .fetch_one(&pool)
-        .await?;
-
-    Ok(record.synergetic_user_id) // TODO: Find out how to get the value of the column in sqlx
-}
-
-pub async fn create_user_by_id(id: i64, synergetic_user_id: i32, mgs_email: &str, mgs_password: &str) -> Result<()> {
-    let pool = establish_database_connection().await?;
-
-    sqlx::query!(
-        "INSERT INTO users (id, synergetic_user_id, mgs_email, mgs_password) VALUES ($1, $2, $3, $4)",
-        id, synergetic_user_id, mgs_email, mgs_password
-    ).execute(&pool).await?;
-
-    Ok(())
-}
-
+// Update
 pub async fn update_user_by_id(id: i64, synergetic_user_id: i32, mgs_email: &str, mgs_password: &str) -> Result<()> {
     let pool = establish_database_connection().await?;
 
@@ -54,6 +51,7 @@ pub async fn update_user_by_id(id: i64, synergetic_user_id: i32, mgs_email: &str
     Ok(())
 }
 
+// Delete
 pub async fn delete_user_by_id(id: i64) -> Result<()> {
     let pool = establish_database_connection().await?;
 
@@ -63,4 +61,22 @@ pub async fn delete_user_by_id(id: i64) -> Result<()> {
     ).execute(&pool).await?;
 
     Ok(())
+}
+
+
+/////////////////////
+// Utility Functions
+/////////////////////
+
+pub async fn get_user_synergetic_id_by_user_id(id: i64) -> Result<i32> {
+    let pool = establish_database_connection().await?;
+
+    let record = sqlx::query!(
+        r#"SELECT synergetic_user_id FROM users WHERE id = $1"#,
+            id
+        )
+        .fetch_one(&pool)
+        .await?;
+
+    Ok(record.synergetic_user_id) // TODO: Find out how to get the value of the column in sqlx
 }
