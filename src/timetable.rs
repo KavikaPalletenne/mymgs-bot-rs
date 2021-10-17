cfd75ce3eff59fd33be49de0f975d846fb699d52
Kavika <kbpalletenne@gmail.com>
2021-10-17 20:46:50 +1100

    implemented CRUD for Timetable struct

--- src/timetable.rs
+++ src/timetable.rs
@@ -2,12 +2,14 @@ use crate::persistence::establish_database_connection;
 use crate::auth::login;
 use crate::models::Timetable;
 use crate::user;
+use crate::class;
 use sqlx::*;
 use std::env;
 use dotenv::dotenv;
 use serde_json;
 use hyper::{Client, Request, Body};
 use hyper_tls::HttpsConnector;
+use chrono::NaiveDate;
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
 
@@ -47,6 +49,70 @@ pub async fn fetch_timetable_by_synergetic_id(synergetic_id: i32) -> Result<()> 
 
     println!("{}", json[2][1]["ClassCodeDescription"]); // Gets day 1, period 1
     //TODO: Get json out of response
+
+    Ok(())
+}
+
+
+
+//////////////////
+// CRUD Functions
+/////////////////
+
+// Create
+pub async fn create_timetable(user_id: i64, fetched_date: NaiveDate) -> Result<()> {
+    let pool = establish_database_connection().await?;
+
+    sqlx::query!(
+        "INSERT INTO timetables (user_id, fetched_date)
+         VALUES ($1, $2)",
+        user_id, fetched_date
+    ).execute(&pool).await?;
+
+    Ok(())
+}
+
+// Read
+pub async fn get_timetable_by_user_id(user_id: i64) -> Result<Timetable> {
+    let pool = establish_database_connection().await?;
+
+    let timetable = sqlx::query_as!(
+        Timetable,
+        "SELECT * FROM timetables WHERE user_id = $1",
+        user_id
+    ).fetch_one(&pool).await?;
+
+    Ok(timetable)
+}
+
+// Update
+pub async fn update_timetable_by_user_id(user_id: i64, fetched_date: NaiveDate) -> Result<()> {
+    let pool = establish_database_connection().await?;
+
+    sqlx::query!(
+        "UPDATE timetables SET fetched_date = $1 WHERE user_id = $2",
+        fetched_date, user_id
+    ).execute(&pool).await?;
+
+    Ok(())
+}
+
+// Delete - Also delete all classed associated with the timetable
+pub async fn delete_timetable_by_user_id(user_id: i64) -> Result<()> {
+    let pool = establish_database_connection().await?;
+
+    // Get timetable id to use in deleting classes
+    let timetable_id = sqlx::query!(
+        r#"SELECT id FROM timetables WHERE user_id = $1"#,
+        user_id
+    ).fetch_one(&pool).await?.id;
+
+    class::delete_all_classes_in_timetable(timetable_id).await?;
+
+    sqlx::query!(
+        "DELETE FROM timetables WHERE user_id = $1",
+        user_id
+    ).execute(&pool).await?;
 
     Ok(())
 }
