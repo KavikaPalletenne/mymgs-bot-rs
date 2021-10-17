99ac8a350f1da5ac70fc1fe81677c1d42ebf75af
Kavika <kbpalletenne@gmail.com>
2021-10-17 20:31:19 +1100

    removed deprecated NewType Structs

--- src/models.rs
+++ src/models.rs
@@ -9,15 +9,6 @@ pub struct User {
     pub mgs_password: Option<String>,
 }
 
-// #[derive(Insertable)]
-// #[table_name="users"]
-// pub struct NewUser<'a> {
-//     pub id: &'a i64,
-//     pub synergetic_user_id: &'a i32,
-//     pub mgs_email: Option<&'a str>,
-//     pub mgs_password: Option<&'a str>,
-// }
-
 
 #[derive(Debug, Serialize, Deserialize, sqlx::FromRow)]
 pub struct Timetable {
@@ -26,35 +17,14 @@ pub struct Timetable {
     fetched_date: NaiveDate,
 }
 
-// #[derive(Insertable)]
-// #[table_name="timetables"]
-// pub struct NewTimetable<'a> {
-//     pub id: &'a i32,
-//     pub user_id: &'a i64,
-//     pub fetched_date: &'a NaiveDate,
-// }
 
-
-// TODO: Connect to timetable with diesel::associations
 #[derive(Debug, Serialize, Deserialize, sqlx::FromRow)]
 pub struct Class {
-    id: i32,
-    timetable_id: i32,
-    day_number: i16, // Day 1 - 7
-    period_number: i16, // The first period of the day would have value "1"
+    pub id: i32,
+    pub timetable_id: i32,
+    pub day_number: i16, // Day 1 - 7
+    pub period_number: i16, // The first period of the day would have value "1"
 
-    name: String,
-    teacher: String,
+    pub name: String,
+    pub teacher: String,
 }
-
-// #[derive(Insertable)]
-// #[table_name="classes"]
-// pub struct NewClass<'a> {
-//     pub id: &'a i32,
-//     pub timetable_id: &'a i32,
-//     pub day_number: &'a i16, // Day 1 - 7
-//     pub period_number: &'a i16, // The first period of the day would have value "1"
-//
-//     pub name: &'a str,
-//     pub teacher: &'a str,
-// }
